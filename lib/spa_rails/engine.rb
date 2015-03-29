50d0c8253e7045807521bbe3df027eb8e64bb2c1
Rodik <i6@bk.ru>
2015-03-29 13:33:35 +0300

    Slim config for angular support

--- lib/spa_rails/engine.rb
+++ lib/spa_rails/engine.rb
@@ -3,6 +3,7 @@ require "slim"
 module SpaRails
   class Engine < ::Rails::Engine
     initializer 'spa-rails', group: :all  do |app|
+      Slim::Engine.set_options(attr_list_delims: {'(' => ')', '[' => ']'})
       app.assets.register_engine('.slimpage', Slim::Template)
     end
   end
