d3e8353d4c68b4fbcc60aec1a8da3969352251a6
Rodik <i6@bk.ru>
2015-04-03 05:07:33 +0300

    requires fix2

--- lib/spa_rails.rb
+++ lib/spa_rails.rb
@@ -1,7 +1,7 @@
 require "uglifier"
 require "sprockets/railtie"
-require "sass/rails/railtie"
-require "ngannotate/rails/railtie"
+require "sass/rails"
+require "ngannotate/rails"
 
 require "spa_rails/version"
 require "spa_rails/routes"
