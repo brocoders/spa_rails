e3177045a1c2843461a6fe2ffa8d3ed51bce9860
Rodik <i6@bk.ru>
2015-04-10 11:17:52 +0300

    Ngannotate hack require

--- lib/spa_rails/ngannotate.rb
+++ lib/spa_rails/ngannotate.rb
@@ -2,10 +2,10 @@ require 'ngannotate/processor'
 
 module Ngannotate
   class Processor < Sprockets::Processor
-    alias_method_chain :skip, :in_root
-
     def skip_with_in_root
-      skip_witout_in_root || !@file.start_with?(::Rails.root.to_s)
+      skip_without_in_root || !@file.start_with?(::Rails.root.to_s)
     end
+
+    alias_method_chain :skip, :in_root
   end
 end
